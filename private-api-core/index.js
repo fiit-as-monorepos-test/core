5b33fbb7be89132b99bc47054d05711085bb9036
Michal Magula <m.magula23@gmail.com>
2023-11-20 22:12:35 +0100

    Update core

--- private-api-core/index.js
+++ private-api-core/index.js
@@ -1,9 +1,3 @@
-import express from "express";
-const app = express();
-export const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello from PRIVATE API CORE endpoint!");
-});
-
-export default app;
+export const coreHandler = (req, res) => {
+  res.send('Hello from PRIVATE API CORE endpoint!');
+};
--- public-api-core/index.js
+++ public-api-core/index.js
@@ -1,9 +1,3 @@
-import express from "express";
-const app = express();
-export const port = 4000;
-
-app.get("/", (req, res) => {
-  res.send("Hello from PUBLIC API CORE endpoint!");
-});
-
-export default app;
+export const coreHandler = (req, res) => {
+  res.send('Hello from PUBLIC API CORE endpoint!');
+};
